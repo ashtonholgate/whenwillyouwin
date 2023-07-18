6f4b5c596843b5ec3bb63b23e6251eb4b02d4b22
ashtonholgate <ashton.holgate@gmail.com>
2023-07-18 10:06:41 +0100

    Added simulationController

---
+++ be/src/controllers/simulation.controller.ts
@@ -0,0 +1,12 @@
+export const simulationController = () => {
+  const startNewSimulation = (
+  ) => {
+    
+  };
+  
+  return {
+    startNewSimulation,
+  };
+};
+
+export type SimulationController = ReturnType<typeof simulationController>;
