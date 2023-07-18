e192b5d4728122dc0977dcfda4a18deabe70a5ce
ashtonholgate <ashton.holgate@gmail.com>
2023-07-18 10:07:38 +0100

    updated simulationRouter

--- be/src/routers/simulation.router.ts
+++ be/src/routers/simulation.router.ts
@@ -5,11 +5,11 @@ import { lotteryTypes } from "../data/lotteryType.data";
 export const simulationRouter = (
 	generatorService: GeneratorService,
 ) => {
-	const generateNewLottery = async (req: Request, res: Response) => {
+	const startNewLottery = async (req: Request, res: Response) => {
 		const data = generatorService.generateLotteryRounds(10000, new Date(), lotteryTypes.lotto);
 		return res.status(200).json(data);
 	};
 
 	return Router()
-		.get("/", generateNewLottery);
+		.get("/", startNewLottery);
 };
